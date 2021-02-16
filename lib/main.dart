4d722ea5ce6498fedf8f0885dd7c2e660c3596aa
KChristelle <r.ayateke@alustudent.com>
2021-02-16 13:25:28 +0200

    Updated to Plant me version

--- lib/main.dart
+++ lib/main.dart
@@ -26,48 +26,72 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      // backgroundColor: Colors.green[700],
       resizeToAvoidBottomPadding: false,
       body: Column(
+        //Entire page will be in one column
+        // Align "Hello there" to the left side of the screen
         crossAxisAlignment: CrossAxisAlignment.start,
+
         children: <Widget>[
           Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                  child: Text(
-                    'Hello',
-                    style:
-                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+            // stack that will have the words "Hello there"
+            child: Center(
+              child: Stack(
+                children: <Widget>[
+                  // For each container padding will inform their position
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 110.0, 0.0, 0.0),
+                    child: Text(
+                      'Plant Me',
+                      style: TextStyle(
+                          fontSize: 80.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green[900]),
+                    ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(15.0, 185.0, 0.0, 0.0),
-                  child: Text(
-                    'There',
-                    style:
-                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(120.0, 215.0, 0.0, 0.0),
+                    child: SizedBox(
+                      height: 10.0,
+                      width: 150.0,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(color: Colors.green[900]),
+                      ),
+                    ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(250.0, 185.0, 0.0, 0.0),
-                  child: Text(
-                    '.',
-                    style: TextStyle(
-                        fontSize: 80.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.greenAccent),
-                  ),
-                ),
-              ],
+
+                  // Container(
+                  //   padding: EdgeInsets.fromLTRB(15.0, 185.0, 0.0, 0.0),
+                  //   child: Text(
+                  //     'There',
+                  //     style: TextStyle(
+                  //         fontSize: 80.0,
+                  //         fontWeight: FontWeight.bold,
+                  //         color: Colors.green[900]),
+                  //   ),
+                  // ),
+                  // Container(
+                  //   padding: EdgeInsets.fromLTRB(250.0, 185.0, 0.0, 0.0),
+                  //   child: Text(
+                  //     '.',
+                  //     style: TextStyle(
+                  //         fontSize: 80.0,
+                  //         fontWeight: FontWeight.bold,
+                  //         color: Colors.greenAccent),
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
           ),
-          // Starting of the sign in form
 
+          // Starting of the sign in form
           Container(
             padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
             child: Column(
               children: <Widget>[
+                // first text field "Email"
                 TextField(
                   decoration: InputDecoration(
                       labelText: 'EMAIL',
@@ -78,9 +102,11 @@ class _MyHomePageState extends State<MyHomePage> {
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.green))),
                 ),
+                // create space between the textfields
                 SizedBox(
                   height: 20.0,
                 ),
+                // second text field "Password"
                 TextField(
                   decoration: InputDecoration(
                       labelText: 'PASSWORD',
@@ -93,19 +119,34 @@ class _MyHomePageState extends State<MyHomePage> {
                   obscureText: true,
                 ),
                 SizedBox(
-                  height: 20.0,
+                  height: 40.0,
                 ),
-                Container(
-                  alignment: Alignment(1.0, 1.0),
-                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                  child: InkWell(
-                    child: Text('Forgot Password',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.greenAccent,
-                            decoration: TextDecoration.underline)),
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      child: InkWell(
+                        child: Text('Create an account',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green[600],
+                                decoration: TextDecoration.underline)),
+                      ),
+                    ),
+                    Container(
+                      // alignment: Alignment(-1.0, -1.0),
+                      // padding: EdgeInsets.only(top: 15.0, left: 15.0),
+                      child: InkWell(
+                        child: Text('Forgot Password',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green[600],
+                                decoration: TextDecoration.underline)),
+                      ),
+                    ),
+                  ],
                 ),
+
                 SizedBox(
                   height: 40.0,
                 ),
@@ -114,7 +155,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Material(
                     borderRadius: BorderRadius.circular(20.0),
                     shadowColor: Colors.grey[700],
-                    color: Colors.green,
+                    color: Colors.green[900],
                     elevation: 7.0,
                     child: GestureDetector(
                       onTap: () {},
